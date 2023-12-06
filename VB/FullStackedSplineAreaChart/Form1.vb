1b8c0be604a16f311a9281292d7a73d9489c7298
DevExpressExampleBot <fakeEmail>
2023-12-06 18:51:16 +0400

    Source auto update [skip ci]

--- VB/FullStackedSplineAreaChart/Form1.vb
+++ VB/FullStackedSplineAreaChart/Form1.vb
@@ -1,24 +1,23 @@
 Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
-' ...
 
+' ...
 Namespace FullStackedSplineAreaChart
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
+
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) _
-        Handles MyBase.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' Create an empty chart.
-            Dim fullStackedSplineAreaChart As New ChartControl()
-
+            Dim fullStackedSplineAreaChart As ChartControl = New ChartControl()
             ' Create two series of the FullStackedSplineArea view type.
-            Dim series1 As New Series("Series 1", ViewType.FullStackedSplineArea)
-            Dim series2 As New Series("Series 2", ViewType.FullStackedSplineArea)
-
+            Dim series1 As Series = New Series("Series 1", ViewType.FullStackedSplineArea)
+            Dim series2 As Series = New Series("Series 2", ViewType.FullStackedSplineArea)
             ' Populate both series with points.
             series1.Points.Add(New SeriesPoint("A", 80))
             series1.Points.Add(New SeriesPoint("B", 20))
@@ -28,27 +27,21 @@ Namespace FullStackedSplineAreaChart
             series2.Points.Add(New SeriesPoint("B", 60))
             series2.Points.Add(New SeriesPoint("C", 20))
             series2.Points.Add(New SeriesPoint("D", 80))
-
             ' Add the series to the chart.
             fullStackedSplineAreaChart.Series.AddRange(New Series() {series1, series2})
-
             ' Adjust the view-type-specific options of the series.
             CType(series1.View, FullStackedSplineAreaSeriesView).LineTensionPercent = 80
             CType(series2.View, FullStackedSplineAreaSeriesView).Transparency = 80
-
             ' Access the diagram's options.
             CType(fullStackedSplineAreaChart.Diagram, XYDiagram).Rotated = True
-
             ' Add a title to the chart and hide the legend.
-            Dim chartTitle1 As New ChartTitle()
+            Dim chartTitle1 As ChartTitle = New ChartTitle()
             chartTitle1.Text = "Full Stacked Spline Area Chart"
             fullStackedSplineAreaChart.Titles.Add(chartTitle1)
             fullStackedSplineAreaChart.Legend.Visible = False
-
             ' Add the chart to the form.
             fullStackedSplineAreaChart.Dock = DockStyle.Fill
             Me.Controls.Add(fullStackedSplineAreaChart)
         End Sub
     End Class
-
 End Namespace
